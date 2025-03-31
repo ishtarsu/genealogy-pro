199e01f5858bbf43b257be33658bd3d92f6f5e8e
ishtarsu <156864960+ishtarsu@users.noreply.github.com>
2025-04-01 00:26:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///genealogie.db'
